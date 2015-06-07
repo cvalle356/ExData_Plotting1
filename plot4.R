c542e2c74eba91cd4c5f1af8aad0d7093a5be9b3
cvalle356 <cvalle356@gmail.com>
2015-06-07 14:47:07 -0700

    Updated the 4th plot
    noticed an error on plot 4 and updated code

--- plot4.R
+++ plot4.R
@@ -3,8 +3,8 @@ sub <- power[power$Date %in% c("1/2/2007","2/2/2007") ,]
 
 #create plot 4
 datetime <- strptime(paste(sub$Date, sub$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
-gloactpo <- as.numeric(subSetData$Global_active_power)
-gloactpo <- as.numeric(subSetData$Global_reactive_power)
+gloactpo <- as.numeric(sub$Global_active_power)
+gloreactpo <- as.numeric(sub$Global_reactive_power)
 volt <- as.numeric(sub$Voltage)
 submet1 <- as.numeric(sub$Sub_metering_1)
 submet2 <- as.numeric(sub$Sub_metering_2)
@@ -22,6 +22,6 @@ lines(datetime, submet2, type="l", col="red")
 lines(datetime, submet3, type="l", col="blue")
 legend("topright", c("sub_metering_1", "sub_metering_2", "sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
 
-plot(datetime, gloactpo, type="l", xlab="datetime", ylab="Global_reactive_power")
+plot(datetime, gloreactpo, type="l", xlab="datetime", ylab="Global_reactive_power")
 
 dev.off()
